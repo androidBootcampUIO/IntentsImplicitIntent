4c52c9e0a228e8923042eeacc768bcccef2a52ff
Diego Villacís <diegovillacis101@gmail.com>
2017-09-18 22:14:57 -0500

    Agregar intent de telefono

--- app/src/main/java/uio/androidbootcamp/intentsimplicitintent/MainActivity.java
+++ app/src/main/java/uio/androidbootcamp/intentsimplicitintent/MainActivity.java
@@ -2,17 +2,21 @@ package uio.androidbootcamp.intentsimplicitintent;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageView;
+    private EditText phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button cameraButton = (Button) findViewById(R.id.camera_button);
+        Button phoneButton = (Button) findViewById(R.id.phone_button);
 
+        phoneNumber = (EditText) findViewById(R.id.phone_number);
         imageView = (ImageView) findViewById(R.id.image_view);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +34,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 capturePhoto();
 
+            }
+        });
+
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isValidPhoneNumber()) {
+                    dialPhoneNumber(phoneNumber.getText().toString());
+                } else {
+                    Toast.makeText(MainActivity.this, "Empty phone number", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -46,6 +63,19 @@ public class MainActivity extends AppCompatActivity {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (cameraIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private boolean isValidPhoneNumber() {
+        return !phoneNumber.getText().toString().equals("");
+    }
+
+    private void dialPhoneNumber(String phoneNumber) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:" + phoneNumber));
+
+        if (phoneIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(phoneIntent);
         }
     }
 }
