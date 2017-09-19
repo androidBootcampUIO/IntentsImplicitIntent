2fe8c86120d15abc9b17ff0c9e024543d5d4469a
Diego Villacís <diegovillacis101@gmail.com>
2017-09-18 22:10:58 -0500

    Agregar intent de camara

--- app/src/main/java/uio/androidbootcamp/intentsimplicitintent/MainActivity.java
+++ app/src/main/java/uio/androidbootcamp/intentsimplicitintent/MainActivity.java
@@ -1,13 +1,51 @@
 package uio.androidbootcamp.intentsimplicitintent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cameraButton = (Button) findViewById(R.id.camera_button);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                capturePhoto();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap thumbnail = data.getParcelableExtra("data");
+            imageView.setImageBitmap(thumbnail);
+            imageView.setVisibility(View.VISIBLE);
+
+        }
+    }
+
+    private void capturePhoto() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
